76578ece42d5e06ac7e03abebae4225d6f47f1ef
lkunxyz <lkunemail@126.com>
2025-08-04 12:56:59 +0800

    Update theme.tsx

--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -20,7 +20,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme={process.env.NEXT_PUBLIC_DEFAULT_THEME || "system"}
+      defaultTheme={process.env.NEXT_PUBLIC_DEFAULT_THEME || "dark"}
       enableSystem
       disableTransitionOnChange
     >
